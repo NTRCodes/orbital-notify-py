e64ffa4222890aaa0f5424d7071f3c98927331a6
nr44c <nigel.ross85@gmail.com>
2026-04-21 11:27:43 -0700

    remove uvicorn from main for now (#1)

--- app/main.py
+++ app/main.py
@@ -1,8 +1,3 @@
 from app import create_app
 
 app = create_app()
-
-# Only run with `uvicorn app.main:create_app` in dev mode
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run("app:create_app", reload=True)
